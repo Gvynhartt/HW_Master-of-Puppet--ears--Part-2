96df69c3bb7621e57e032cdcacc3a62a927bdbe5
Gvynhartt <savshush134@gmail.com>
2023-08-18 23:53:19 +0300

    minor edits in task #1

--- nutflex.test.js
+++ nutflex.test.js
@@ -1,26 +1,31 @@
 // custom command imports
 const { default: puppeteer } = require("puppeteer");
-const {getRandomIntIncl, selectNdaysFromToday, clickPageElement, selectRandomSeat, selectRandomHallByType} = 
-require("./lib/custom_commands.js");
+const {
+  getRandomIntIncl,
+  selectNdaysFromToday,
+  clickPageElement,
+  selectRandomSeat,
+  selectRandomHallByType,
+} = require("./lib/custom_commands.js");
 
-const { KnownDevices } = require ("puppeteer");
-const iPhoneSE = KnownDevices['iPhone SE'];
+const { KnownDevices } = require("puppeteer");
+const iPhoneSE = KnownDevices["iPhone SE"];
 
 let page;
 
-describe("Cinema booking site tests, Happy Path", () => { // блок describe, для которого будут работать хуки 
-  
+describe("Cinema booking site tests, Happy Path", () => {
+  // блок describe, для которого будут работать хуки
+
   beforeEach(async () => {
     page = await browser.newPage();
     await page.goto("http://qamid.tmweb.ru/client/index.php");
   });
-  
+
   afterEach(() => {
     page.close();
   });
 
   test("Happy path #1: book a standard ticket", async () => {
-
     const targetDay = 3;
     // выбираем сеансы спустя три дня
     const selDayChosen = "a.page-nav__day_chosen";
@@ -28,8 +33,6 @@ describe("Cinema booking site tests, Happy Path", () => { // блок describe, 
     await page.waitForSelector(selDayChosen);
 
     await selectRandomHallByType(page, "standard");
-    // const urlHallPage = window.location.href;
-    // expect(urlHallPage).toEqual("http://qamid.tmweb.ru/client/hall.php");
     const selSeatScheme = "div.buying-scheme";
     await page.waitForSelector(selSeatScheme);
 
@@ -40,24 +43,25 @@ describe("Cinema booking site tests, Happy Path", () => { // блок describe, 
     await clickPageElement(page, selBookSeatBtn);
     const selTicketHeader = "header h2.ticket__check-title";
     await page.waitForSelector(selTicketHeader);
-    // const urlPaymentPage = window.location.href;
-    // expect(urlPaymentPage).toEqual("http://qamid.tmweb.ru/client/payment.php");
 
-    const selQRcodeReceiveBtn = "div.ticket__info-wrapper button.acceptin-button";
+    const selQRcodeReceiveBtn =
+      "div.ticket__info-wrapper button.acceptin-button";
     await clickPageElement(page, selQRcodeReceiveBtn);
 
     const selQRticketHeader = "header h2.ticket__check-title";
     await page.waitForSelector(selQRticketHeader);
-    // const urlTicketPage = window.location.href;
-    // expect(urlTicketPage).toEqual("http://qamid.tmweb.ru/client/ticket.php");
 
     await page.waitForSelector("div.ticket__info-wrapper img.ticket__info-qr");
-    const ticketHint = await page.$eval("div.ticket__info-wrapper :nth-child(7)", element => element.textContent);
-    expect(ticketHint).toEqual("Покажите QR-код нашему контроллеру для подтверждения бронирования.");
+    const ticketHint = await page.$eval(
+      "div.ticket__info-wrapper :nth-child(7)",
+      (element) => element.textContent
+    );
+    expect(ticketHint).toEqual(
+      "Покажите QR-код нашему контроллеру для подтверждения бронирования."
+    );
   }, 10000);
 
   test("Happy path #2: book a VIP ticket", async () => {
-
     const targetDay = 1;
     const selDayChosen = "a.page-nav__day_chosen";
     await selectNdaysFromToday(page, targetDay);
@@ -78,24 +82,29 @@ describe("Cinema booking site tests, Happy Path", () => { // блок describe, 
     const selTicketHeader = "header h2.ticket__check-title";
     await page.waitForSelector(selTicketHeader);
 
-    const selQRcodeReceiveBtn = "div.ticket__info-wrapper button.acceptin-button";
+    const selQRcodeReceiveBtn =
+      "div.ticket__info-wrapper button.acceptin-button";
     await clickPageElement(page, selQRcodeReceiveBtn);
 
     const selQRticketHeader = "header h2.ticket__check-title";
     await page.waitForSelector(selQRticketHeader);
 
     await page.waitForSelector("div.ticket__info-wrapper img.ticket__info-qr");
-    const ticketHint = await page.$eval("div.ticket__info-wrapper :nth-child(7)", element => element.textContent);
-    expect(ticketHint).toEqual("Покажите QR-код нашему контроллеру для подтверждения бронирования.");
+    const ticketHint = await page.$eval(
+      "div.ticket__info-wrapper :nth-child(7)",
+      (element) => element.textContent
+    );
+    expect(ticketHint).toEqual(
+      "Покажите QR-код нашему контроллеру для подтверждения бронирования."
+    );
   }, 10000);
 });
 
 test("Sad path #1: book a standard ticket on mobile device", async () => {
-
   const page = await browser.newPage();
   await page.emulate(iPhoneSE);
   await page.goto("http://qamid.tmweb.ru/client/index.php");
-  
+
   const targetDay = 2;
   const selDayChosen = "a.page-nav__day_chosen";
   await selectNdaysFromToday(page, targetDay);
@@ -111,17 +120,17 @@ test("Sad path #1: book a standard ticket on mobile device", async () => {
 
   function isElementHidden(page, selector, timeout) {
     return new Promise((resolve) => {
-        page.waitForSelector(selector, {visible: false, timeout}).then(() => {
-            resolve(true);
-        }).catch(() => {
-            resolve(false);
+      page
+        .waitForSelector(selector, { visible: false, timeout })
+        .then(() => {
+          resolve(true);
+        })
+        .catch(() => {
+          resolve(false);
         });
     });
-}
- const isBookButtonHidden = await isElementHidden(page, selBookSeatBtn, 500);
-  
+  }
+  const isBookButtonHidden = await isElementHidden(page, selBookSeatBtn, 500);
+
   expect(isBookButtonHidden).toEqual(true);
-
-  // await browser.close();
 }, 10000);
-
